65b82e75d47477958b52f27b2619ba52e4267368
forana <alex@catalyze.io>
2014-06-30 13:23:48 -0500

    github pages push

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+// Global config object with keys for the two APIs we'll be using.
+Config = {
+    catalyze_apikey: "browser demo 4b7852d1-3228-4c35-a166-2e98b8a64909",
+    medlio_apikey: "97e074261ef4c8007c84037874788a88"
+};
